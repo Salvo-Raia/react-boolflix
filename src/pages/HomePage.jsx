438ad806e2380c2fb31dcb702b0ea4811aa85de3
Salvo-Raia <salvo.raia@outlook.it>
2026-02-24 17:43:28 +0100

    Add: languageToFlag function

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,15 @@
 import { useSearch } from "../contexts/SearchContext";
+import "flag-icons/css/flag-icons.min.css";
+
+const languages = {
+  en: <span class="fi fi-gb"></span>,
+  it: <span class="fi fi-it"></span>,
+  ja: <span class="fi fi-jp"></span>,
+};
+
+function languageToFlag(languageId) {
+  return languages[languageId] || "Info non disponibile";
+}
 
 export default function HomePage() {
   const { movieList, tvSeriesList } = useSearch();
@@ -9,24 +20,24 @@ export default function HomePage() {
       <div className="row row-cols-4 g-2 my-5">
         {movieList.map((movie) => (
           <div key={movie.id} className="col">
-            <div className="movie-card">
+            <div className="movie-card border p-3">
               <h3>{movie.title}</h3>
-              <h4>{movie.original_title}</h4>
-              <p>{movie.original_language}</p>
-              <p>{movie.vote_average}</p>
+              <h4 className="text-muted">{movie.original_title}</h4>
+              <p>Lingua originale: {languageToFlag(movie.original_language)}</p>
+              <p>Voto: {movie.vote_average}</p>
             </div>
           </div>
         ))}
       </div>
-      <h2>Serie TV</h2>
+      <h2>Serie TV </h2>
       <div className="row row-cols-4 my-5">
         {tvSeriesList.map((tv) => (
           <div key={tv.id} className="col">
-            <div className="movie-card">
+            <div className="movie-card border p-3">
               <h3>{tv.name}</h3>
-              <h4>{tv.original_name}</h4>
+              <h4 className="fs-8 text-muted">{tv.original_name}</h4>
               <p>{tv.original_language}</p>
-              <p>{tv.vote_average}</p>
+              <p>Voto: {tv.vote_average}</p>
             </div>
           </div>
         ))}
