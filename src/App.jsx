bfdc6d002a1c6bc52c91dc17d11c7801a95b76bf
Salvo-Raia <salvo.raia@outlook.it>
2026-02-24 15:55:33 +0100

    Init repository + Add layout and routing

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+import HomePage from "./pages/HomePage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route Component={DefaultLayout}>
+          <Route path="/" Component={HomePage} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
