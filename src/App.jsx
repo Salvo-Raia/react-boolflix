08703fa519abe1013c60416a8433fd196c900cd0
Salvo-Raia <salvo.raia@outlook.it>
2026-02-24 16:51:49 +0100

    Add: axios get request

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+import { SearchProvider } from "./contexts/SearchContext";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DefaultLayout from "./layouts/DefaultLayout";
 import HomePage from "./pages/HomePage";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route Component={DefaultLayout}>
-          <Route path="/" Component={HomePage} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <SearchProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={DefaultLayout}>
+            <Route path="/" Component={HomePage} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </SearchProvider>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,58 @@
-import { useSearch } from "../contexts/SearchContext";
+import { useState } from "react";
+import axios from "axios";
+
 //Base API URL//
 const BASE_API_URL = "https://api.themoviedb.org/3/search/";
 const API_KEY = "8b9baf966f3d0d2de6f7bc2cc9417531";
-//Movie API URL //
-const movieApiURL = new URL(BASE_API_URL + "movie");
-movieApiURL.searchParams.set("api_key", API_KEY);
-movieApiURL.searchParams.set("query", "velluto");
-movieApiURL.searchParams.set("language", "it-IT");
-//TV Series API URL //
-const tvApiURL = new URL(BASE_API_URL + "tv");
-tvApiURL.searchParams.set("api_key", API_KEY);
-tvApiURL.searchParams.set("query", "preacher");
-tvApiURL.searchParams.set("language", "it-IT");
 
 export default function Header() {
+  const [query, setQuery] = useState("");
+
+  //Movie API URL //
+  const movieApiURL = new URL(BASE_API_URL + "movie");
+  movieApiURL.searchParams.set("api_key", API_KEY);
+  movieApiURL.searchParams.set("query", query);
+  movieApiURL.searchParams.set("language", "it-IT");
+  //TV Series API URL //
+  const tvApiURL = new URL(BASE_API_URL + "tv");
+  tvApiURL.searchParams.set("api_key", API_KEY);
+  tvApiURL.searchParams.set("query", query);
+  tvApiURL.searchParams.set("language", "it-IT");
+
+  // Fetch film e serie tv
+  const fetchMovies = () => {
+    axios.get(movieApiURL).then((res) => {
+      console.log(res.data);
+    });
+  };
+
+  const fetchTvSeries = () => {
+    axios.get(tvApiURL).then((res) => {
+      console.log(res.data);
+    });
+  };
+
+  //Controllo Submit//
+  const handleSearch = (e) => {
+    e.preventDefault();
+    fetchMovies();
+    fetchTvSeries();
+  };
+
   return (
-    <nav class="navbar bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand">Navbar</a>
-        <form class="d-flex" role="search">
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand">Navbar</a>
+        <form className="d-flex" role="search" onSubmit={handleSearch}>
           <input
-            class="form-control me-2"
+            className="form-control me-2"
             type="search"
-            placeholder="Search"
-            aria-label="Search"
+            placeholder="Cerca"
+            aria-label="Cerca"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
           />
-          <button class="btn btn-outline-success">Search</button>
+          <button className="btn btn-outline-success">Search</button>
         </form>
       </div>
     </nav>
